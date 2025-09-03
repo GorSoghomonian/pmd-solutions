cd604b373667cd19732c34d7a84fe6e336cea3a7
GorSoghomonian <drouzzer9@gmail.com>
2025-09-03 17:55:09 +0400

    some details

--- src/components/organsim/ServiceFooter.js
+++ src/components/organsim/ServiceFooter.js
@@ -1,3 +1,4 @@
+"use client";
 import { useTranslations } from 'next-intl';
 import { popularIntegrations, integrationBenefits } from '../../data/homeItems';
 import ActionButtons from '../molecules/ActionButtons';
